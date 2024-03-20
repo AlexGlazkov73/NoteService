b42b3ec3a429ff88099fb40d33f9764f4f9de841
AlexGlazkov73 <zenin7395@mail.ru>
2024-03-20 19:10:46 +0300

    fix: change default hosts in config

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-DB_HOST = env.get('DB_HOST') or '127.0.0.1'
-DB_PORT = env.get('DB_PORT') or 5584
+DB_HOST = env.get('DB_HOST') or '0.0.0.0'
+DB_PORT = env.get('DB_PORT') or 5432
 DB_NAME = env.get('DB_NAME') or 'note_service'
 DB_USER = env.get('DB_USER') or 'note_service'
 DB_PASSWORD = env.get('DB_PWD') or 'note_service'
@@ -12,5 +12,5 @@ DB_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_
 
 DB_SCHEMA = 'note_service'
 
-HOST = env.get('HOST') or '127.0.0.1'
+HOST = env.get('HOST') or '0.0.0.0'
 PORT = env.get('PORT') or 8000
